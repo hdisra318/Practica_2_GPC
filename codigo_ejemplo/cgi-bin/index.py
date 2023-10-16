95164e17fa8d08c1889eaecdeb2d62023edbcba8
hdisra318 <hdisra318@ciencias.unam.mx>
2023-10-15 21:04:08 -0600

    Descomentando cambios

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 import cgi
 
-print ("Content-type: text/html")
-# print
+# print ("Content-type: text/html")
+# # print
 
-print ("""
-Hola Mundo
-""")
+# print ("""
+# Hola Mundo
+# """)
 
-form=cgi.FieldStorage()
-print ("<p>user:", form["user"].value)
-print ("<p>pass:", form["pass"].value)
+# form=cgi.FieldStorage()
+# print ("<p>user:", form["user"].value)
+# print ("<p>pass:", form["pass"].value)
 
-# import psycopg2
-# import bcrypt
+import psycopg2
+import bcrypt
 
 # ---------------- CAMBIOS ANTERIORES ----------------
 # Conectarse a la base de datos
@@ -54,39 +54,39 @@ print ("<p>pass:", form["pass"].value)
 #     print("Credenciales incorrectas")
 # ----------------------------------------------------------------
 
-# print("Content-type: text/html\n")
-# print("""
-# <!DOCTYPE html>
-# <html lang="en">
-# <head>
-#     <meta chaset="UTF-8">
-#     <meta name="viewport" content="widtg=device-width, initial-scale=1.0>
-#     <title>Iniciar Sesión</title>
-#     <link rel="stylesheet" href="/css/styles.css">
-# </head>
-# <body>
-#     <div id="login-container">
-#     """)
-# form=cgi.FieldStorage()
-# try:
-#     connection = psycopg2.connect(user = "admin",
-#                                   password = "losfifas",
-#                                   host = "10.0.0.4",
-#                                   port = 5432,
-#                                   database = "Usuarios")
-#     cursor = connection.cursor()
-#     user = form["user"].value
-#     passw = form["pass"].value
-#     cursor.execute("SELECT password FROM Usuarios WHERE username = %s", (user,))
-#     password_hash = cursor.fetchone()[0].encode('utf8')
+print("Content-type: text/html\n")
+print("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chaset="UTF-8">
+    <meta name="viewport" content="widtg=device-width, initial-scale=1.0>
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <div id="login-container">
+    """)
+form=cgi.FieldStorage()
+try:
+    connection = psycopg2.connect(user = "admin",
+                                  password = "losfifas",
+                                  host = "10.0.0.4",
+                                  port = 5432,
+                                  database = "Usuarios")
+    cursor = connection.cursor()
+    user = form["user"].value
+    passw = form["pass"].value
+    cursor.execute("SELECT password FROM Usuarios WHERE username = %s", (user,))
+    password_hash = cursor.fetchone()[0].encode('utf8')
 
-#     if bcrypt.checkpw(passw.encode('utf8'), password_hash):
-#         print("<h2> Bienvenido :D " + user + " </h2>")
-#     else:
-#         print("<h2> Ese nombre no me suena D: </h2>")
-# except (Exception, psycopg2.Error) as error :
-#     print("Error while connectiong to PostgreSQL", error)
-# finally:
-#     if (connection):
-#         cursor.close()
-#         connection.close()
+    if bcrypt.checkpw(passw.encode('utf8'), password_hash):
+        print("<h2> Bienvenido :D " + user + " </h2>")
+    else:
+        print("<h2> Ese nombre no me suena D: </h2>")
+except (Exception, psycopg2.Error) as error :
+    print("Error while connectiong to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
