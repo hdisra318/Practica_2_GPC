242980c85eb35570d294d2c17532ba01ac1de52e
hdisra318 <hdisra318@ciencias.unam.mx>
2023-10-15 21:54:19 -0600

    Cambios para lab

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 import cgi
 
-# print ("Content-type: text/html")
+print ("Content-type: text/html")
 # # print
 
-# print ("""
-# Hola Mundo
-# """)
+print ("""
+Hola Mundo
+""")
 
-# form=cgi.FieldStorage()
-# print ("<p>user:", form["user"].value)
-# print ("<p>pass:", form["pass"].value)
+form=cgi.FieldStorage()
+print ("<p>user:", form["user"].value)
+print ("<p>pass:", form["pass"].value)
 
-import psycopg2
+#import psycopg2
 # import bcrypt
 
 # ---------------- CAMBIOS ANTERIORES ----------------
@@ -54,20 +54,20 @@ import psycopg2
 #     print("Credenciales incorrectas")
 # ----------------------------------------------------------------
 
-print("Content-type: text/html\n")
-print("""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta chaset="UTF-8">
-    <meta name="viewport" content="widtg=device-width, initial-scale=1.0>
-    <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-    <div id="login-container">
-      <h1>Resultados del Login: </h1>
-    """)
+# print("Content-type: text/html\n")
+# print("""
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta chaset="UTF-8">
+#     <meta name="viewport" content="widtg=device-width, initial-scale=1.0>
+#     <title>Iniciar Sesión</title>
+#     <link rel="stylesheet" href="/css/styles.css">
+# </head>
+# <body>
+#     <div id="login-container">
+#       <h1>Resultados del Login: </h1>
+#     """)
 # form=cgi.FieldStorage()
 # try:
 #     connection = psycopg2.connect(user = "admin",
@@ -93,8 +93,8 @@ print("""
 #         connection.close()
 
 
-print("""
-    </div>
-</body>
-</html>
-""")
+# print("""
+#     </div>
+# </body>
+# </html>
+# """)
