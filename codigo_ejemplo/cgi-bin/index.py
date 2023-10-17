cb826bf6567a800a3b3cd37e94524e9fc5e4d679
hdisra318 <hdisra318@ciencias.unam.mx>
2023-10-16 21:11:56 -0600

    Cambios de nombre de la BD 3

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -88,9 +88,10 @@ try:
     cursor.execute("SELECT password FROM Usuarios WHERE username = %s", (user,))
     print("<p> Linea despues del SELECT - TRY")
     # password_hash = cursor.fetchone()[0].encode('utf8')
-    password = cursor.fetchone()
+    password = cursor.fetchone()[0].encode('utf8')
     cursor.close()
     connection.close()
+    print(password == None)
     print("<p>"+password+"</p>")
 #     if bcrypt.checkpw(passw.encode('utf8'), password_hash):
 #         print("<h2> Bienvenido :D " + user + " </h2>")
