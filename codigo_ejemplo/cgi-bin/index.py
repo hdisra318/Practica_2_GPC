3fb7099c355cab00609ab642c33aef88ff1b2250
hdisra318 <hdisra318@ciencias.unam.mx>
2023-10-16 23:16:52 -0600

    Arreglando problemas de los estilos

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -64,7 +64,7 @@ print("""
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <div class="login-info">
