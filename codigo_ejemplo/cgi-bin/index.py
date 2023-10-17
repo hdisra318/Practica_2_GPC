9f2c1c26cd2b6e01e47a5fb8ad786b4d6f5fe40c
hdisra318 <hdisra318@ciencias.unam.mx>
2023-10-16 23:15:31 -0600

    Arreglando conexion ala BD 3

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -61,8 +61,8 @@ print("""
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta chaset="UTF-8">
-    <meta name="viewport" content="widtg=device-width, initial-scale=1.0>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
