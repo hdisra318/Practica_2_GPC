e6c8f23f27446ca9dde963ac7d4510bd3be048b4
dimoc666 <diegomartinez@ciencias.unam.mx>
2023-10-21 00:26:58 -0600

    Se linea para agregar usuario 3

--- codigo_ejemplo/cgi-bin/index.py
+++ codigo_ejemplo/cgi-bin/index.py
@@ -85,13 +85,13 @@ try:
                                   database = "usuarios")
     cursor = connection.cursor()
 
-    def registrar_usuario(username, password):
-        # Generar un hash de la contraseña
-        password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    # def registrar_usuario(username, password):
+    #     # Generar un hash de la contraseña
+    #     password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-        # Insertar el usuario en la base de datos
-        cursor.execute("INSERT INTO Usuarios (username, password) VALUES (%s, %s)", (username, password))
-        connection.commit()
+    #     # Insertar el usuario en la base de datos
+    #     cursor.execute("INSERT INTO Usuarios (username, password) VALUES (%s, %s)", (username, password))
+    #     connection.commit()
 
     # cursor.execute("SELECT password FROM Usuarios WHERE username = %s", (user,))
     # Consulta parametrizada
@@ -115,9 +115,9 @@ try:
                     </div>
                 </div>
                 """)
-            registrar_usuario('diego', 'contr321G666')
-            registrar_usuario('israel', 'otracontrasena123')
-            registrar_usuario('israel', 'otracontrasena123')
+            # registrar_usuario('diego', 'contr321G666')
+            # registrar_usuario('israel', 'otracontrasena123')
+            # registrar_usuario('israel', 'otracontrasena123')
         else:
             print("""
                 <div class="login-container login-fail">
